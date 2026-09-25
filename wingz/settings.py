80a481af94dd96fc7346e45ece3a73da61e00752
Gizelle <gizelle.dev@gmail.com>
2026-09-25 15:44:01 +0800

    Add support for postgres

--- wingz/settings.py
+++ wingz/settings.py
@@ -79,12 +79,18 @@ AUTH_USER_MODEL = "users.User"
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("DATABASE_URL"):
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(default=os.environ["DATABASE_URL"], conn_max_age=600),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
