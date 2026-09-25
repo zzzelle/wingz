f8495096039a6c848628289c0a461e988d545cce
Gizelle <gizelle.dev@gmail.com>
2026-09-26 00:45:40 +0800

    Add setting of password in User update

--- users/serializers.py
+++ users/serializers.py
@@ -14,6 +14,15 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
     class Meta:
         model = User
         fields = [
